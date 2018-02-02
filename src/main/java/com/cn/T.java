cca6a70c25cce5b4b9e55cc422a25864cbde233c
hangonglian <hgl_eagle@163.com>
2018-02-02 18:01:19 +0800

    wers

---
+++ src/main/java/com/cn/T.java
@@ -0,0 +1,18 @@
+package com.cn;
+
+/**
+ * file:T
+ * <p>
+ * 文件简要说明
+ *
+ * @author 2018年02月02日  韩龚连  创建初始版本
+ * @version 2018年02月02日  V1.0  简要版本说明
+ * @par 版权信息：
+ * 2018 Copyright 河南艾鹿网络科技有限公司 All Rights Reserved.
+ */
+public class T {
+    public static void main(String[] args){
+        String str = "aaaaaa";
+        
+    }
+}
